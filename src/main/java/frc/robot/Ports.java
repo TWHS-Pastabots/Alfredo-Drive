8dd3b982223184b55c4c38fdd20e4275495d38f9
jprock23 <juanproque@outlook.com>
2023-10-03 14:13:43 -0500

    Created some objects for interacting with motor/encoders

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Ports {
+
+    public static final int LEFT_SPEED_FRONT = 1;
+    public static final int LEFT_SPEED_BACK = 2;
+    public static final int RIGHT_SPEED_FRONT = 3;
+    public static final int RIGHT_SPEED_BACK = 4;
+
+    public static final int LEFT_ANGLE_FRONT = 5;
+    public static final int LEFT_ANGLE_BACK = 6;
+    public static final int RIGHT_ANGLE_FRONT = 7;
+    public static final int RIGHT_ANGLE_BACK = 8;
+
+    public static final int GYRO = 9;
+}
